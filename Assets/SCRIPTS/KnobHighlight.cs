f04db326044417ab34d469e09152b24d5dfb7ba5
Happy Tony <happytony_su@outlook.com>
2026-03-05 17:57:00 +0100

    Knob Collider

--- Assets/SCRIPTS/KnobHighlight.cs
+++ Assets/SCRIPTS/KnobHighlight.cs
@@ -1,24 +1,36 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class KnobHighlight : MonoBehaviour
 {
     public Renderer knobRenderer;
-    public Color highlightColor = Color.yellow;
+    public Color highlightColor;
 
     private Color originalColor;
 
     void Start()
     {
         originalColor = knobRenderer.material.color;
+       
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
+
+
+        if (other.transform.root.name == "Cupboard_B")
+            return; // ignore cupboard itself
+
+        UnityEngine.Debug.Log($"[KnobTrigger] ENTER by: {other.name} | root: {other.transform.root.name}");
+
         if (other.CompareTag("Hand"))
         {
             knobRenderer.material.color = highlightColor;
+           
         }
+        
     }
+
 
     void OnTriggerExit(Collider other)
     {
