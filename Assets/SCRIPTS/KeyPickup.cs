a91bea87253963fba0a140ecd35e3f1d2f9d5549
Happy Tony <happytony_su@outlook.com>
2026-03-10 13:45:45 +0100

    Revert "Game Manager Update"
    This reverts commit 95c034063130f5377d3d880c620bb6b6edf616e7.

--- Assets/SCRIPTS/KeyPickup.cs
+++ Assets/SCRIPTS/KeyPickup.cs
@@ -3,22 +3,11 @@ using UnityEngine;
 public class KeyPickup : MonoBehaviour
 {
     public PlayerInventory inventory;
-    private GameManager gm;
-
-    void Start()
-    {
-        gm = FindFirstObjectByType<GameManager>();
-    }
 
     public void PickUpKey()
     {
         inventory.hasKey = true;
         Debug.Log("Key collected!");
-
-        if (gm != null)
-        {
-            gm.PlayerGotKey();
-        }
 
         Destroy(gameObject);
     }
